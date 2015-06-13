4c53cf5184bddfd4c62d90414b41df858e6561d1
Gareth Loader <garethloader@gmail.com>
2015-06-13 12:06:29 +0100

    Update GNU

--- RX/RX.ino
+++ RX/RX.ino
@@ -1,8 +1,24 @@
-// Garage door detector receiver code
-// Twitter @gareth_loader
-// Various parts copied from other examples from whom I have forgotten
+/*
+    Garage door detector receiver code
 
-//  433mhz Receiver = pin 11
+    <Gareth Loader>  @gareth_loader
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Various parts have been copied from other examples from whom I have forgotten
+
+*/
 
 
 #include <LowPower.h>
