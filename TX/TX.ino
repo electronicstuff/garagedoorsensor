2fec1fbda43cff49557ddb52f177f9e54b0cc825
Gareth Loader <gareth@electronicstuff.info>
2015-12-17 19:49:48 +0000

    Update TX.ino

--- TX/TX.ino
+++ TX/TX.ino
@@ -2,7 +2,7 @@
 
     Garage door detector transmitter code
 
-    <Gareth Loader>  @gareth_loader
+    <Gareth Loader>  www.electronicstuff.info
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
